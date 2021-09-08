92dc433bd1dfc8ed1633b25fa7b7e0b39873f10d
Ilya Mahin <zubastu@gmail.com>
2021-09-08 15:05:56 +0300

    JS_complete_work

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-let popup = document.querySelector('.popup'); //создаю обьект через который буду воздействовать на разметку проекта.
+
+    let formElement = document.querySelector('.form')
+
+    let nameInput = document.querySelector('.form__input_heading');
+    let jobInput = document.querySelector('.form__input_subheading')
+
+    let closeButton = document.querySelector('.form__close-button');
+    let profileButton = document.querySelector('.profile__info-button');
+    let popup = document.querySelector('.popup_opened');
 
 
+    function popupOpened () {
+        popup.setAttribute('style', 'display: flex;');
+        console.log('check_button1');
+    }
 
+    function popupClose() {
+        popup.setAttribute('style', 'display: none;');
+    }
 
+    function formSubmitHandler (evt) {
+        evt.preventDefault();
+        let name = document.querySelector('.profile__info-heading');
+        let job = document.querySelector('.profile__info-subheading');
+        name.textContent = nameInput.value;
+        job.textContent = jobInput.value;
+        popupClose();
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log(popup);
+    formElement.addEventListener('submit', formSubmitHandler);
+    closeButton.addEventListener('click', popupClose);
+    profileButton.addEventListener('click', popupOpened);
