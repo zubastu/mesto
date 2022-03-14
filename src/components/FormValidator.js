1c000624027f197cf5a0d0d4c92c48081fddcfad
zubastu <79757684+zubastu@users.noreply.github.com>
2022-03-14 23:24:15 +0300

    Исправил класс Валидации

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -7,65 +7,66 @@ export default class FormValidator {
         this._inactiveButtonClass = data.inactiveButtonClass;
         this._inputErrorClass = data.inputErrorClass;
         this._errorClass = data.errorClass;
+        this._inputList = Array.from(this._form.querySelectorAll(this._inputSelector));
+        this._buttonElement = this._form.querySelector(this._submitButtonSelector);
     }
 
     enableValidation() {
-        this._setEventListeners(this._form);
+        this._setEventListeners();
     }
 
-    _setEventListeners(formElement) {
-        const inputList = Array.from(formElement.querySelectorAll(this._inputSelector));
-        const buttonElement = formElement.querySelector(this._submitButtonSelector);
-        this.toggleButtonState(inputList, buttonElement);
-        inputList.forEach((inputElement) => {
+    _setEventListeners() {
+        this.toggleButtonState();
+        this._inputList.forEach((inputElement) => {
             inputElement.addEventListener('input', () => {
-                this._checkInputValidity(formElement, inputElement);
-                this.toggleButtonState(inputList, buttonElement);
+                this._checkInputValidity(inputElement);
+                this.toggleButtonState();
             });
         });
     }
 
-    hideInputError(formElement, inputElement) {
-        const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    hideInputError(inputElement) {
+        const errorElement = this._form.querySelector(`.${inputElement.id}-error`);
         inputElement.classList.remove(this._inputErrorClass);
         errorElement.classList.remove(this._errorClass);
         errorElement.textContent = '';
     }
 
-    _showInputError(formElement, inputElement, errorMessage) {
-        const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    _showInputError(inputElement, errorMessage) {
+        const errorElement = this._form.querySelector(`.${inputElement.id}-error`);
         inputElement.classList.add(this._inputErrorClass);
         errorElement.textContent = errorMessage;
         errorElement.classList.add(this._errorClass);
     }
 
-    _checkInputValidity(formElement, inputElement) {
+    _checkInputValidity(inputElement) {
         if (!inputElement.validity.valid) {
-            this._showInputError(formElement, inputElement, inputElement.validationMessage);
+            this._showInputError(inputElement, inputElement.validationMessage);
         } else {
-            this.hideInputError(formElement, inputElement);
+            this.hideInputError(inputElement);
         }
     }
 
-    _hasInvalidInput(inputList) {
-        return inputList.some((inputElement) => {
+    _hasInvalidInput() {
+        return this._inputList.some((inputElement) => {
             return !inputElement.validity.valid;
         });
     }
 
-    clearErrorMessages(formElement, inputList) {
-        inputList.forEach( (input) => {
-            this.hideInputError(formElement, input);
+    clearErrorMessages() {
+        this._inputList.forEach((input) => {
+            this.hideInputError(input);
         });
+        this.toggleButtonState();
     }
 
-    toggleButtonState(inputList, buttonElement){
-        if (this._hasInvalidInput(inputList)) {
-            buttonElement.setAttribute('disabled', true);
-            buttonElement.classList.add(this._inactiveButtonClass);
+    toggleButtonState(){
+        if (this._hasInvalidInput()) {
+            this._buttonElement.setAttribute('disabled', true);
+            this._buttonElement.classList.add(this._inactiveButtonClass);
         } else {
-            buttonElement.classList.remove(this._inactiveButtonClass);
-            buttonElement.removeAttribute('disabled');
+            this._buttonElement.classList.remove(this._inactiveButtonClass);
+            this._buttonElement.removeAttribute('disabled');
         }
     }
 
